3fc013beaa313c05a8097a37b1beb14ddaade08c
droesch09 <danielrosch@Daniels-MacBook-Pro-2.local>
2017-01-26 23:57:58 +0100

    empty message

--- index.js
+++ index.js
@@ -287,8 +287,8 @@ function receivedMessage(event) {
     // If we receive a text message, check to see if it matches any special
     // keywords and send back the corresponding example. Otherwise, just echo
     // the text we received.
-    switch (messageText) {
-      case includes('location'):
+    switch messageText {
+      case messageText.includes('location'):
         sendLocationMessage(senderID);
         break;
 
