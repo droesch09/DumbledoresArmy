b1441fe7c5d1f8e4fc23b9ae1a53f1a54ad52184
droesch09 <danielrosch@Daniels-MacBook-Pro-2.local>
2017-01-27 01:53:58 +0100

    empty message

--- index.js
+++ index.js
@@ -281,7 +281,18 @@ function receivedMessage(event) {
     sendTextMessage(senderID, "Oh. Darauf antworten wir dir am besten persönlich. Wir werden uns schnellstmöglich bei dir melden.");
   }
 
-
+if (messageText) {
+    if (messageText.includes('location')){
+        sendLocationMessage(senderID);
+    }
+    else {
+        sendStartMessage(SenderID);
+    }
+}
+} else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+    
 if (messageText) {
 
     // If we receive a text message, check to see if it matches any special
